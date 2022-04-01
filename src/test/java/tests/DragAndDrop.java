c4f2c2e02c35ade4bc8f487660844a35195642b2
Elizaveta.Polyakova <polyakova.elizaveta95@mail.ru>
2022-04-01 21:42:18 +0300

    added drag and drop test

---
+++ src/test/java/tests/DragAndDrop.java
@@ -0,0 +1,25 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDrop {
+
+    @BeforeAll
+    static void beforeAll() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        Configuration.browserSize="1920x1080";
+    }
+
+    @Test
+    void CheckDragAndDrop() {
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
